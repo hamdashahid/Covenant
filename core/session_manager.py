21f7f9e31a05c85057309e4244882436a030494d
Manahil Ramzan <muhammad.ramzan.2367@gmail.com>
2026-07-27 16:23:29 +0500

    bs kuch

--- core/session_manager.py
+++ core/session_manager.py
@@ -45,7 +45,7 @@ class SessionManager:
             "applicant_profile": {},
             "profile_conflicts": [],
             "turn_count": 0,
-            "max_turns": 8,
+            "max_turns": 8,#commit
             "decision_status": "Requires More Info",
             "decision_summary": "",
         }
